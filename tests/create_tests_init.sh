34a464fcf24deb2dcddc070b1998372cfa288983
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-02-14 20:25:22 +0900

    tests part

---
+++ tests/create_tests_init.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+for d in ./src/*/;
+  do
+    echo $d
+
+    test_dir="tests/test_${d:6:${#d}}"
+    init_py="${test_dir}__init__.py"
+
+    if [ ! -d "$test_dir" ]; then
+       mkdir ${test_dir}
+       echo $test_dir
+    fi
+
+    if [ ! -f "${init_py}" ]; then
+       touch ${init_py}
+       echo $init_py
+    fi
+
+  done
