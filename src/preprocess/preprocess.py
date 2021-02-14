25b5f9f1763f2aa7b830129805e562d028f1ac00
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-02-14 15:10:29 +0900

    passed formatter

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,23 +1,17 @@
-import pandas as pd
-
-import yaml
 from pathlib import Path
 
-import glob
+import pandas as pd
+import yaml
+
 
 if __name__ == "__main__":
     path_config = Path("src/config/config.yaml")
 
-    with open(path_config, mode='r') as f:
+    with open(path_config, mode="r") as f:
         config = yaml.load(f, Loader=yaml.SafeLoader)
 
     df = pd.read_csv(
-        './data/raw/breast-cancer-wisconsin.data',
-        names=config["feature_names"]
-        )
+        "./data/raw/breast-cancer-wisconsin.data", names=config["feature_names"]
+    )
 
-    df.to_csv("./data/interim/data.csv",
-        encoding="utf-8",
-        index=False
-        )
-
+    df.to_csv("./data/interim/data.csv", encoding="utf-8", index=False)
